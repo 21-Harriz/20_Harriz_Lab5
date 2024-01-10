e9bbeedd9c29b4248c69809a0e3dc8ba841509a4
Alvin <155930198+tanyanyii@users.noreply.github.com>
2024-01-10 12:27:19 +0800

    Updated Unity Project
    Updated by yanyi

---
+++ 20_Harriz_Lab 5/20_Harriz_Lab 5/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody playerRigidbody;
+    public GameObject coin;
+    public Text CoinCollected;
+    public int coincount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CoinCollected.text = "Coins Collected = " + coincount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CoinCollected.text = "Coins Collected = " + coincount;
+        if (coincount >= 4)
+        {
+            SceneManager.LoadScene("GamePlay_Level2");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(MoveHorizontal, 0, MoveVertical);
+        transform.Translate(movement * Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coins")
+        {
+            Destroy(other.gameObject);
+            coincount++;
+            
+        }
+        if (other.tag == "Hazard")
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+}
